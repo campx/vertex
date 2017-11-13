65003d802909b066f277a0b25746f4403ab69307
markovchainy <markovchainy@users.noreply.github.com>
2017-11-13 19:53:38 +0000

    Added support for different compilers

--- include/vertex/Predicate.h
+++ include/vertex/Predicate.h
@@ -1,31 +1,29 @@
 #pragma once
-#include <utility>
 #include <cstdint>
 #include <map>
+#include <utility>
 
 namespace vertex
 {
 
-template <typename VertexMap>
-struct Predicate
+/** A predicate which always returns either true or false regardless of the
+ * input */
+template <typename VertexMap, bool Result>
+struct NullaryPredicate
 {
     using result_type = bool;
     using key_type = typename VertexMap::key_type;
     using argument_type = std::pair<key_type, key_type>;
-};
-
-/** A predicate which always returns either true or false regardless of the
- * input */
-template <typename VertexMap, bool Result>
-struct NullaryPredicate : public Predicate<VertexMap>
-{
     bool operator()(const argument_type& value) const;
 };
 
 template <typename VertexMap>
-class MaxDepthPredicate : public Predicate<VertexMap>
+class MaxDepthPredicate
 {
 public:
+    using result_type = bool;
+    using key_type = typename VertexMap::key_type;
+    using argument_type = std::pair<key_type, key_type>;
     MaxDepthPredicate(uint64_t max_depth);
     bool operator()(const argument_type& value);
 
@@ -38,7 +36,7 @@ private:
 template <typename VertexMap, bool Result>
 bool NullaryPredicate<VertexMap, Result>::
 operator()(const typename NullaryPredicate<VertexMap, Result>::argument_type&
-    value) const
+               value) const
 {
     (void)value;
     return Result;
@@ -51,8 +49,8 @@ MaxDepthPredicate<VertexMap>::MaxDepthPredicate(uint64_t max_depth)
 }
 
 template <typename VertexMap>
-bool MaxDepthPredicate<VertexMap>::operator()(
-    const typename MaxDepthPredicate<VertexMap>::argument_type& value)
+bool MaxDepthPredicate<VertexMap>::
+operator()(const typename MaxDepthPredicate<VertexMap>::argument_type& value)
 {
     auto it = sources_.find(value.first);
     auto depth = uint64_t(it == sources_.end() ? 0 : it->second);
@@ -60,5 +58,4 @@ bool MaxDepthPredicate<VertexMap>::operator()(
     sources_.insert(std::make_pair(value.second, depth + 1));
     return depth < max_depth_;
 }
-
 }
