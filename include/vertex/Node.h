91ea32ec6de3f7c358bc3c66c64e80b8808c9ec7
markovchainy <markovchainy@users.noreply.github.com>
2018-01-31 08:42:13 +0000

    Added links accessor method to node

--- include/vertex/Node.h
+++ include/vertex/Node.h
@@ -52,7 +52,7 @@ public:
     /** Get the number of children */
     size_type length() const;
 
-    /** Check whether the vertex has any children */
+    /** Check whether the node has any children */
     bool empty() const;
 
     /** Remove all children from the Node */
@@ -61,26 +61,32 @@ public:
     /** Exchanges the contents of the Node with those of other */
     void swap(Node& other);
 
-    /** Insert a child vertex */
+    /** Insert a child node */
     std::pair<iterator, bool> insert(const_iterator pos, const Link& child);
 
     /** Insert a range of children */
     template <typename InputIt>
     iterator insert(iterator pos, InputIt first, InputIt last);
 
-    /** Find a child vertex */
+    /** Find a child node */
     iterator find(const Link& child);
 
-    /** Find a child vertex */
+    /** Find a child node */
     const_iterator find(const Link& child) const;
 
-    /** Remove a child vertex */
+    /** Remove a child node */
     iterator erase(const_iterator child);
 
-    /** Get the data associated with this vertex */
+    /** Get the out-bound links from this node */
+    const container_type& links() const;
+
+    /** Set the out-bound links for this node */
+    void links(const container_type& value);
+
+    /** Get the data associated with this node */
     const value_type& data() const;
 
-    /** Set the data associated with this vertex */
+    /** Set the data associated with this node */
     void data(const value_type& value);
 
     /** Compare the contents and children of a Node against another */
@@ -91,7 +97,7 @@ public:
 
 private:
     value_type data_;
-    container_type children_;
+    container_type links_;
 };
 
 template <typename Link, typename T>
@@ -102,61 +108,61 @@ Node<Link, T>::Node(value_type data) : data_(std::move(data))
 template <typename Link, typename T>
 typename Node<Link, T>::iterator Node<Link, T>::begin()
 {
-    return children_.begin();
+    return links_.begin();
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::iterator Node<Link, T>::end()
 {
-    return children_.end();
+    return links_.end();
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::const_iterator Node<Link, T>::begin() const
 {
-    return children_.begin();
+    return links_.begin();
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::const_iterator Node<Link, T>::end() const
 {
-    return children_.end();
+    return links_.end();
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::const_iterator Node<Link, T>::cbegin() const
 {
-    return children_.cbegin();
+    return links_.cbegin();
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::const_iterator Node<Link, T>::cend() const
 {
-    return children_.cend();
+    return links_.cend();
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::size_type Node<Link, T>::length() const
 {
-    return children_.size();
+    return links_.size();
 }
 
 template <typename Link, typename T>
 bool Node<Link, T>::empty() const
 {
-    return children_.empty();
+    return links_.empty();
 }
 
 template <typename Link, typename T>
 void Node<Link, T>::clear()
 {
-    return children_.clear();
+    return links_.clear();
 }
 
 template <typename Link, typename T>
 void Node<Link, T>::swap(Node& other)
 {
-    std::swap(children_, other.children_);
+    std::swap(links_, other.links_);
     std::swap(data_, other.data_);
 }
 
@@ -169,7 +175,7 @@ Node<Link, T>::insert(typename Node<Link, T>::const_iterator pos,
     auto do_insert = it == end();
     if (do_insert)
     {
-        it = children_.insert(pos, child);
+        it = links_.insert(pos, child);
     }
     auto result = std::make_pair(it, do_insert);
     return result;
@@ -182,13 +188,13 @@ Node<Link, T>::insert(typename Node<Link, T>::iterator pos,
                       InputIt first,
                       InputIt last)
 {
-    return children_.insert(pos, first, last);
+    return links_.insert(pos, first, last);
 }
 
 template <typename Link, typename T>
 typename Node<Link, T>::iterator Node<Link, T>::find(const Link& child)
 {
-    auto it = std::find(children_.begin(), children_.end(), child);
+    auto it = std::find(links_.begin(), links_.end(), child);
     return it;
 }
 
@@ -196,7 +202,7 @@ template <typename Link, typename T>
 typename Node<Link, T>::const_iterator
 Node<Link, T>::find(const Link& child) const
 {
-    auto it = std::find(children_.cbegin(), children_.cend(), child);
+    auto it = std::find(links_.cbegin(), links_.cend(), child);
     return it;
 }
 
@@ -204,7 +210,19 @@ template <typename Link, typename T>
 typename Node<Link, T>::iterator
 Node<Link, T>::erase(typename Node<Link, T>::const_iterator child)
 {
-    return children_.erase(child);
+    return links_.erase(child);
+}
+
+template <typename Link, typename T>
+const typename Node<Link, T>::container_type& Node<Link, T>::links() const
+{
+    return links_;
+}
+
+template <typename Link, typename T>
+void Node<Link, T>::links(const typename Node<Link, T>::container_type& value)
+{
+    links_ = value;
 }
 
 template <typename Link, typename T>
@@ -222,7 +240,7 @@ void Node<Link, T>::data(const typename Node<Link, T>::value_type& value)
 template <typename Link, typename T>
 bool Node<Link, T>::operator==(const Node<Link, T>& rhs) const
 {
-    return data_ == rhs.data_ && children_ == rhs.children_;
+    return data_ == rhs.data_ && links_ == rhs.links_;
 }
 
 template <typename Link, typename T>
