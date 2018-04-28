df9a41ccef1f2b6c0bdffb8de99f5fa33edda1df
markovchainy <markovchainy@users.noreply.github.com>
2018-04-28 08:59:42 +0100

    Implemented find method of PathMap

--- vertex/PathMap.h
+++ vertex/PathMap.h
@@ -1,9 +1,12 @@
 #pragma once
 #include <algorithm>
 #include <functional>
+#include <toolbox/Iterator.h>
+#include <toolbox/IteratorRecorder.h>
+#include <toolbox/IteratorTransformer.h>
+#include <toolbox/SequencePredicate.h>
 #include <vector>
 #include <vertex/BreadthFirstTraversal.h>
-#include <vertex/PathPredicate.h>
 
 namespace vertex
 {
@@ -11,43 +14,90 @@ namespace vertex
 /** PathMap traverses a graph, matching a Link sequence using a given
  * Compare function, returning an array of matching Link,Node pairs */
 template <typename Container,
-          typename Iterator,
           typename Compare = std::equal_to<typename Container::key_type>>
 class PathMap
 {
 public:
-    using const_iterator = typename Container::const_iterator;
-    using value_type = typename Container::value_type;
-    using result_type = std::vector<value_type>;
-    PathMap(const Container& nodes, const_iterator root);
+    using key_type = std::vector<typename Container::key_type>;
+    using mapped_type = typename Container::mapped_type;
+    using value_type = std::pair<key_type, mapped_type>;
 
-    result_type operator()(const Iterator& begin, const Iterator& end) const;
+    class Decoder
+    {
+    public:
+        Decoder() = default;
+        Decoder(key_type path);
+        value_type operator()(const typename Container::value_type& value);
+
+    private:
+        key_type path_;
+    };
+
+    using Predicate =
+        toolbox::SequencePredicate<typename key_type::const_iterator, Compare>;
+    using Traversal = BreadthFirstTraversal<Container, Predicate>;
+    using Transformer = toolbox::IteratorTransformer<Decoder, Traversal>;
+    using iterator = toolbox::IteratorRecorder<Transformer>;
+
+    PathMap(const Container& nodes,
+            typename Container::const_iterator root,
+            Compare compare = Compare());
+    const typename Container::const_iterator& root() const;
+
+    iterator find(const key_type& p) const;
+    iterator erase(const iterator& it);
+    iterator insert(iterator hint, const value_type& value);
 
 private:
     const Container* nodes_;
-    const_iterator root_;
+    typename Container::const_iterator root_;
+    Compare compare_;
 };
 
-template <typename Container, typename Iterator, typename Compare>
-PathMap<Container, Iterator, Compare>::PathMap(
-    const Container& nodes, typename Container::const_iterator root)
-    : nodes_(&nodes), root_(std::move(root))
+template <typename Container, typename Compare>
+PathMap<Container, Compare>::PathMap(const Container& nodes,
+                                     typename Container::const_iterator root,
+                                     Compare compare)
+    : nodes_(&nodes), root_(std::move(root)), compare_(std::move(compare))
 {
 }
 
-template <typename Container, typename Iterator, typename Compare>
-typename PathMap<Container, Iterator, Compare>::result_type
-PathMap<Container, Iterator, Compare>::
-operator()(const Iterator& begin, const Iterator& end) const
+template <typename Container, typename Compare>
+PathMap<Container, Compare>::Decoder::Decoder(key_type path)
+    : path_(std::move(path))
 {
-    using Predicate = PathPredicate<Iterator, Compare>;
-    using Traversal = BreadthFirstTraversal<Container, Predicate>;
-    auto predicate = Predicate(begin, end);
+}
+
+template <typename Container, typename Compare>
+typename PathMap<Container, Compare>::value_type
+PathMap<Container, Compare>::Decoder::
+operator()(const typename Container::value_type& value)
+{
+    path_.push_back(value.first);
+    return std::make_pair(path_, value.second);
+}
+
+template <typename Container, typename Compare>
+const typename Container::const_iterator&
+PathMap<Container, Compare>::root() const
+{
+    return root_;
+}
+
+template <typename Container, typename Compare>
+typename PathMap<Container, Compare>::iterator
+PathMap<Container, Compare>::find(const key_type& p) const
+{
+    auto predicate = Predicate(p.begin(), p.end(), compare_);
     auto first = Traversal(*nodes_, root_, predicate);
     auto last = first.end();
     first = first == last ? first : ++first; // don't include root in results
-    auto result = result_type{};
-    std::copy(first, last, std::back_inserter<result_type>(result));
+    auto decoder = Decoder(key_type{root()->first});
+    auto first_transformer = Transformer(first, decoder);
+    auto last_transformer = Transformer(last, decoder);
+    auto begin = iterator(first_transformer);
+    auto end = iterator(last_transformer);
+    auto result = toolbox::back(begin, end);
     return result;
 }
 
